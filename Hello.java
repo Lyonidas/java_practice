09f0b550d40b240029ba3a0d11c309c9dcb540aa
Preston Hudson <prestonleehudson@gmail.com>
2021-01-30 17:58:02 -0600

    Changed myFirstNumber value to 10.

--- Hello.java
+++ Hello.java
@@ -3,9 +3,9 @@ public class Hello {
   public static void main(String[] args) {
       System.out.println("Hello Preston!");
 
-      int myFirstNumber = 5;
+      int myFirstNumber = 10;
 
       System.out.println(myFirstNumber);
-      
+
   }
 }
