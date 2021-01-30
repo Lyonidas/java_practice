a410f81e1e03ea28a85fe894a998364d6e918031
Preston Hudson <prestonleehudson@gmail.com>
2021-01-30 17:57:30 -0600

    Added a print statement to print the value of myFirstNumber.

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello {
   public static void main(String[] args) {
       System.out.println("Hello Preston!");
 
-      int myNumber = 5;
+      int myFirstNumber = 5;
+
+      System.out.println(myFirstNumber);
+      
   }
 }
