8f50ecebe03590dea27c65c882a8a33c1183d7be
Preston Hudson <prestonleehudson@gmail.com>
2021-01-30 18:53:48 -0600

    Updated myThirdNumber value. Changed print statement to show my total.

--- Hello.java
+++ Hello.java
@@ -5,10 +5,10 @@ public class Hello {
 
       int myFirstNumber = (10 + 5) + (2 * 10);
       int mySecondNumber = 12;
-      int myThirdNumber = 6;
+      int myThirdNumber = myFirstNumber * 2;
       int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
-      System.out.println(myFirstNumber);
+      System.out.println(myTotal);
 
   }
 }
