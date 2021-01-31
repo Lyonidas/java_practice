72e7131c463c9dff7c9eefd0025943025ab8efe8
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 17:41:58 -0600

    FIxed value bug.

--- Float-And-Double/PoundToKilogram.java
+++ Float-And-Double/PoundToKilogram.java
@@ -2,7 +2,7 @@ public class PoundToKilogram {
 
   public static void main(String[] args) {
 
-    double myPounds = 190;
+    double myPound = 190;
     double myKilogram = myPound * 0.453592;
     System.out.println(myKilogram);
 
