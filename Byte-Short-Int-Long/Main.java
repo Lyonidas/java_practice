41ef84a93561e46719b41675cd0e25cab925539d
Preston Hudson <prestonleehudson@gmail.com>
2021-01-30 19:27:23 -0600

    Created print statements to print out Min and Max values.

--- Byte-Short-Int-Long/Main.java
+++ Byte-Short-Int-Long/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     int myMinIntValue  = Integer.MIN_VALUE;
     int myMaxIntValue = Integer.MAX_VALUE;
-    
+    System.out.println("Integer Minimum Value = " + myMinIntValue);
+    System.out.println("Integer Maximum Value = " + myMaxIntValue);
   }
 }
