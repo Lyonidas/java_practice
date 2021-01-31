1daf6ff742aeeb64e2c39614658990bc8e7eb44d
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 16:49:44 -0600

    Fixed mispelled bug.

--- Float-And-Double/Main.java
+++ Float-And-Double/Main.java
@@ -3,9 +3,9 @@ public class Main {
   public static void main(String[] args) {
 
     float myMinFloatValue = Float.MIN_VALUE;
-    float myMaxFLoatValue = Float.MAX_VALUE;
+    float myMaxFloatValue = Float.MAX_VALUE;
     System.out.println("Float minimum value = " + myMinFloatValue);
     System.out.println("Float maximum value = " + myMaxFloatValue);
-    
+
   }
 }
