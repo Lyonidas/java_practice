8b0838c1d2e3d43ddfceeea2854222d6416151bd
Preston Hudson <prestonleehudson@gmail.com>
2021-01-30 18:01:25 -0600

    Added java operaters to sum up to the value of 35.

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
   public static void main(String[] args) {
       System.out.println("Hello Preston!");
 
-      int myFirstNumber = 10;
+      int myFirstNumber = (10 + 5) + (2 * 10);
 
       System.out.println(myFirstNumber);
 
