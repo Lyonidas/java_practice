6ab2c29c0f32fa155fad732a68475c830687f1c8
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 18:01:11 -0600

    Added a unicode value to print.

--- Char-Boolean/Main.java
+++ Char-Boolean/Main.java
@@ -2,8 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    //char can only hod single characters
+    //char can only hold single characters
     char myChar = 'D';
+    char myUnicodeChar = '\u0044';
+    System.out.println(myChar);
+    System.out.println(myUnicodeChar);
 
   }
 }
