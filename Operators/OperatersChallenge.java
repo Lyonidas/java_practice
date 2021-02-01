85edff509dc4d08f3b43c40b3c9ff26ee168e0ad
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 20:55:51 -0600

    Added remainder value.

--- Operators/OperatersChallenge.java
+++ Operators/OperatersChallenge.java
@@ -5,6 +5,11 @@ public class OperatersChallenge {
     double myFirstValue = 20.00;
     double mySecondValue = 80.00;
     double myThirdValue = (myFirstValue + mySecondValue) * 100.00;
+    System.out.println("The total is = " + myThirdValue);
+    double theRemainder = myThirdValue % 40.00
+    System.out.println("The remainder is = " + theRemainder);
+    if () {
 
+    }
   }
 }
