a2ecb3934fb14a1365d4588ae1875ecbe8473967
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 18:49:18 -0600

    Added a Int value to a string.

--- Strings/Main.java
+++ Strings/Main.java
@@ -11,6 +11,10 @@ public class Main {
     String numberString = "250.55";
     numberString = numberString + "49.95";
     System.out.println(numberString);
+    String lastString = "10";
+    int myInt = 50;
+    lastString = lastString + myInt;
+    System.out.println("LastString is equal to ") + lastString;
 
 
   }
