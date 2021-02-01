c3ad6f4c2bc1d8972de94ae6ec274f403232c7ca
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 18:46:42 -0600

    Created new number string value.

--- Strings/Main.java
+++ Strings/Main.java
@@ -8,5 +8,10 @@ public class Main {
     System.out.println("myString is equal to " + myString);
     myString = " \u00A9 2021";
     System.out.println("myString is equal to " + myString);
+    String numberString = "250.55";
+    numberString = numberString + "49.95";
+    System.out.println(numberString);
+
+
   }
 }
