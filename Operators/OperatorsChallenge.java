4816f39100c3a13f1db3f1b6460b8fac72818b6a
Preston Hudson <prestonleehudson@gmail.com>
2021-01-31 21:02:00 -0600

    FIxed double bug.

--- Operators/OperatorsChallenge.java
+++ Operators/OperatorsChallenge.java
@@ -6,7 +6,7 @@ public class OperatorsChallenge {
     double mySecondValue = 80.00;
     double myThirdValue = (myFirstValue + mySecondValue) * 100.00;
     System.out.println("The total is = " + myThirdValue);
-    double theRemainder = myThirdValue % 40.00
+    double theRemainder = myThirdValue % 40.00;
     System.out.println("The remainder is = " + theRemainder);
     boolean isNoRemainder = (theRemainder == 0) ? true : false;
     System.out.println("Is no remainder" + isNoRemainder);
